c16dff084da00edadc4da35fec27dc999516379a
Magomed <85867169+Minor95@users.noreply.github.com>
2022-12-17 17:18:49 +0300

    add profile

--- client/src/components/common/home/Main.jsx
+++ client/src/components/common/home/Main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 function Main() {
+  console.log('любовь')
   return (
     <div className='main_container'>
       <Outlet />
+      
     </div>
   )
 }
